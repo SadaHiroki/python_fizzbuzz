8bb12433f4760a24833e1886947fd6aa1edda94c
佐田皓紀 <kamannberu0311@gmail.com>
2022-05-30 13:36:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-for i in range(100):
+for i in range(1,101):
     if i % 15 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
-        print("fizz")
+        print("Fizz")
     elif i % 5 == 0:
-        print("buzz")
+        print("Buzz")
     else:
         print(i)
